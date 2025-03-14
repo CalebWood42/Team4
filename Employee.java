5bda16c01cfdc63611b5ac5922c35a405f65d346
Brendan King <151489184+brendanking3@users.noreply.github.com>
2025-03-13 20:49:02 -0500

    Update Employee.java
    Updated employee class, with similar changes to the customer class. Now, whenever the employee does something they tweet about it inside the Employee class instead of the Demo class.

--- Employee.java
+++ Employee.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
  * add one item at a time to the smart shelf.
  */
 public class Employee {
+	
 	/**
 	 * This method places a list of unsorted items on the shelf.
 	 * 
@@ -14,18 +15,32 @@ public class Employee {
 	 * @param items An unsorted ArrayList of RetailItem objects.
 	 */
 	public void placeItems(IShelfEmployee shelf, ArrayList<RetailItem> items) {
+		System.out.println("----------Employee placing on the shelf:");
+		
+		// Print each item in the unsorted list.
+		for (RetailItem item : items) {
+			System.out.println(item);
+		}
+		
+		// Employee places the items on the shelf.
 		shelf.placeItems(items);
-
+		
+		// Tweet what the employee just did.
+		System.out.println("\n----------Placed on the shelf:");
+		
+		// Display the current items on the shelf.
+		System.out.println(shelf);
 	}
 
 	/**
 	 * This method places a single item on the shelf while maintaining the sorted
-	 * order.
+	 * order.(Unfortunately, it does not maintain the sorted order).
 	 * 
 	 * @param shelf The smart shelf as an IShelfEmployee reference.
 	 * @param item  The single RetailItem to add to the shelf.
 	 */
 	public void addItem(IShelfEmployee shelf, RetailItem item) {
+		System.out.println("----------Employee adding " + item);
 		shelf.addItem(item);
 	}
 }
