755a9988058ee070779a4fd961cc7a285ddecb17
Brendan King <151489184+brendanking3@users.noreply.github.com>
2025-03-13 21:00:34 -0500

    Update IShelfCustomer.java
    Minor changes to javadocs

--- IShelfCustomer.java
+++ IShelfCustomer.java
@@ -5,13 +5,12 @@ package edu.collin.cosc2436.bking.supermarketsmartshelf;
  */
 public interface IShelfCustomer {
 	/**
-	 * This method searches for an item by its name, removes it, and returns a
-	 * reference to an item with this name. If the item cannot be found an
-	 * OutOfStockException is thrown.
+	 * This method takes the name of an item and returns a reference to an item with
+	 * this name. If the item cannot be found an OutOfStockException is thrown.
 	 * 
-	 * @param name The name of the item the customer wants.
-	 * @return The RetailItem if found.
-	 * @throws OutOfStockException if the item is not available on the shelf.
+	 * @param name The name of an item.
+	 * @return A reference to an item with this name, if found.
+	 * @throws OutOfStockException If the item cannot be found.
 	 */
 	RetailItem findAndTake(String name) throws OutOfStockException;
 }
