f405dca95ff525bd45e785cb862c05fc2e1770a8
CalebWood42 <99432270+CalebWood42@users.noreply.github.com>
2025-03-14 15:03:19 -0500

    Update IShelfEmployee.java

--- IShelfEmployee.java
+++ IShelfEmployee.java
@@ -5,19 +5,19 @@ import java.util.ArrayList;
 /**
  * Interface for employee interactions with the smart shelf.
  */
-public interface IShelfEmployee {
+public interface IShelfEmployee <T extends RetailItem>{
 	/**
 	 * This method puts an unsorted ArrayList of items on the shelf.
 	 * 
 	 * @param items An ArrayList of RetailItem objects to be placed on the shelf.
 	 */
-	void placeItems(ArrayList<RetailItem> items);
+	void placeItems(ArrayList<T> items);
 
 	/**
 	 * This method adds a single item to the shelf while maintaining sorted order.
 	 * 
 	 * @param item The RetailItem to add.
 	 */
-	void addItem(RetailItem item);
+	void addItem(T item);
 
 }
